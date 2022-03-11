67df1d34f708978e529ab9114280c6f1bfb22e46
SwanZey <61594963+SwanZey@users.noreply.github.com>
2022-03-11 20:10:20 +0100

    game ??

--- Game/.import/Checking.gd
+++ Game/.import/Checking.gd
@@ -9,6 +9,7 @@ signal dead
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var tileset = self.get_tileset()
+	var tiles = get_used_cells()
 	set_process_input(true)
 
 func _input(event):
@@ -16,10 +17,15 @@ func _input(event):
 	if event.is_action_pressed("click"):
 		if self.get_cellv(tile_pos) != -1:
 			var tile_name = self.tile_set.tile_get_name(self.get_cellv(tile_pos))
-			if tile_name == "3":
-				self.set_cellv(tile_pos, self.get_tileset().find_tile_by_name("1"))
-			elif tile_name == "4":
-				self.set_cellv(tile_pos, self.get_tileset().find_tile_by_name("2"))
+			if tile_name == "2-1":
+				self.set_cellv(tile_pos, self.get_tileset().find_tile_by_name("1-1"))
+			elif tile_name == "2-2":
+				self.set_cellv(tile_pos, self.get_tileset().find_tile_by_name("1-2"))
+			elif tile_name == "2-3":
+				self.set_cellv(tile_pos, self.get_tileset().find_tile_by_name("1-3"))
+			elif tile_name == "2-4":
+				self.set_cellv(tile_pos, self.get_tileset().find_tile_by_name("1-4"))
+			elif tile_name == "wrong":
 				var life = get_parent().life
 				life -= 1
 				get_parent().life = life
